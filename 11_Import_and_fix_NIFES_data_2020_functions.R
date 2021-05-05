3debab1e32f24f039a0897d96b92d1272dc95481
Dag_Hjermann <dhj@niva.no>
2021-05-05 15:22:06 +0200

    Add NIFES data + script 11, 2021 Norwegian Sea

--- 11_Import_and_fix_NIFES_data_2020_functions.R
+++ 11_Import_and_fix_NIFES_data_2020_functions.R
@@ -1,7 +1,7 @@
 #
 # Conditions for functions to work:
 # - Column no 1 is called "Jnr"
-# - Column no 2 is called "Prøvenr."
+# - Column no 2 is called "PrÃ¸venr."
 # - Then there are x more columns (position, species etc.)
 # - Then concentrations, given in column pairs for each substance, where 
 #    + column 1 has concentration
@@ -12,8 +12,10 @@
 
 #
 # Put concentrations on long format
+# Old version. Problem: doesn't include data columns that are not followed by a "Unit" column
+# - such as the second CB118 column  
 #
-nifes_standardize_conc <- function(data){
+nifes_standardize_conc_OLD <- function(data){
   # Next 3 lines is identical in nifes_standardize_conc() and nifes_standardize_unit()
   colnumber_unit <- grep("^Enhet", colnames(data))
   colnumber_conc <- colnumber_unit - 1
@@ -22,7 +24,28 @@ nifes_standardize_conc <- function(data){
   data[c(colnumber_meta, colnumber_conc)] %>%
     pivot_longer(-(Jnr:`Produkt beskrivelse`), names_to = "Parameter", values_to = "Conc_chr")
 }
-# nifes_standardize_conc(dat1) %>% select(Jnr, Prøvenr., Parameter, Conc_chr)
+
+#
+# Put concentrations on long format
+# New version - includes all concentration columns, also those not followed by a "Unit" column
+#   (unless the first concentration column is not followed by a "Unit" column, see 'column_conc_1')
+#
+nifes_standardize_conc <- function(data){
+  # Next 3 lines is identical in nifes_standardize_conc() and nifes_standardize_unit()
+  colnumber_unit <- grep("^Enhet", colnames(data))
+  column_conc_1 <- colnumber_unit[1]-1    # define first colun as the first column followed by a "Unit" column
+  column_conc_2 <- ncol(data)
+  colnumber_conc <- seq(column_conc_1, column_conc_2)
+  colnumber_conc <- colnumber_conc[!colnumber_conc %in% colnumber_unit]
+  colnumber_meta <- 1:(colnumber_conc[1]-1)
+  # Reformat
+  data[c(colnumber_meta, colnumber_conc)] %>%
+    pivot_longer(-(Jnr:`Produkt beskrivelse`), names_to = "Parameter", values_to = "Conc_chr")
+}
+# nifes_standardize_conc(dat1) %>% select(Jnr, PrÃ¸venr., Parameter, Conc_chr)
+
+
+# nifes_standardize_conc(dat1) %>% select(Jnr, PrÃ¸venr., Parameter, Conc_chr)
 
 #
 # Put units on long format
@@ -38,14 +61,14 @@ nifes_standardize_unit <- function(data){
   colnames(data_unit)[-c(1,2)] <- colnames(data)[colnumber_conc]
   # Reformat
   data_unit %>%
-    pivot_longer(-c(Jnr,Prøvenr.), names_to = "Parameter", values_to = "Unit")
+    pivot_longer(-c(Jnr,PrÃ¸venr.), names_to = "Parameter", values_to = "Unit")
 }
-# nifes_standardize_unit(dat3) %>% select(Jnr, Prøvenr., Parameter, Unit)
+# nifes_standardize_unit(dat3) %>% select(Jnr, PrÃ¸venr., Parameter, Unit)
 
 nifes_standardize_data <- function(data){
   data_conc <- nifes_standardize_conc(data)
   data_unit <- nifes_standardize_unit(data)
-  data <- left_join(data_conc, data_unit, by = c("Jnr","Prøvenr.","Parameter"))
+  data <- left_join(data_conc, data_unit, by = c("Jnr","PrÃ¸venr.","Parameter"))
   # Remove less-than sign and make Conc variable
   data <- data %>%
     mutate(
