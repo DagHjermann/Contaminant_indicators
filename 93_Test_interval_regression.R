a47f0f5b644ced22e0a66b11f1d73a6eb46a7781
Dag_Hjermann <dhj@niva.no>
2023-03-30 13:07:39 +0200

    Add JAGS regression for interval-censored data

--- 93_Test_interval_regression.R
+++ 93_Test_interval_regression.R
@@ -1,13 +1,13 @@
 
+#
+# Packages ----
+#
+
 library(dplyr)
 library(tidyr)
 library(readxl)
 library(ggplot2)
 
-# install.packages("icenReg")
-library(icenReg)
-library(foreach)
-library(doParallel)
 
 #
 # Checkng stations and parameters for 202 assessment
@@ -33,7 +33,7 @@ rownames(tab2)
 # "CD"    "DDEPP" "HCB"   "HG"    "PB" 
 
 #
-# Latest NIVA data
+# Latest NIVA data ----
 #
 
 # dat_all <- readRDS("../Milkys2_pc/Files_from_Jupyterhub_2021/Raw_data/101_data_updated_2022-09-01.rds") %>%
@@ -51,7 +51,7 @@ dat_1a <- dat_all %>%
 xtabs(~PARAM + STATION_CODE, dat_1a)
 
 #
-# Calculate upper/lower bounds of PCB7
+# Calculate upper/lower bounds of PCB7 ----
 #
 
 dat_1b <- dat_all %>%
@@ -70,18 +70,48 @@ dat_1b <- dat_all %>%
   )
 
 #
-# Test interval regression using individual data
+# Get sumPCB7 data for one station  
 #
 
+#
+# . Individual data ----
+#
 dat_test1 <- dat_1b %>%
   filter(STATION_CODE == "10A2")
 
 ggplot(dat_test1, aes(x = MYEAR)) +
   geom_point(aes(y = CB7_up), color = "red") +
   geom_point(aes(y = CB7_lo), color = "blue")
-  
+
+#
+# . Median data ----
+#
+dat_test2 <- dat_test1 %>%
+  group_by(STATION_CODE, LATIN_NAME, MYEAR) %>%
+  summarise(
+    CB7_lo = median(CB7_lo),
+    CB7_up = median(CB7_up)
+  )
 
 
+ggplot(dat_test2, aes(x = MYEAR)) +
+  geom_point(aes(y = CB7_up), color = "red") +
+  geom_point(aes(y = CB7_lo), color = "blue")
+
+
+
+#
+# Interval regression using 'icenReg' ----   
+#
+# - NOTE: will not be used
+# 
+
+# install.packages("icenReg")
+library(icenReg)
+library(foreach)
+library(doParallel)
+
+# using individual datat  
 mle_fit <- ic_par(cbind(CB7_lo, CB7_up) ~ MYEAR,
                   model = "ph",
                   dist = "lnorm",
@@ -90,24 +120,68 @@ summary(mle_fit)
 
 
 #
-# Test interval regression using medians per year  
+# Interval regression using JAGS function ----   
 #
 
-dat_test2 <- dat_test1 %>%
-  group_by(STATION_CODE, LATIN_NAME, MYEAR) %>%
-  summarise(
-    CB7_lo = median(CB7_lo),
-    CB7_up = median(CB7_up)
-  )
+library(R2jags)
+library(rjags)
+source("interval_lm_qi.R")  # function 'int_linear_qi'  
+
+#
+# . all data (until 2021) ----      
+#
+
+
+# Model
+mod <- int_linear_qi(data = dat_test2, x = "MYEAR", y_lo = "CB7_lo", y_up = "CB7_up")
+
+# str(mod,1)
+
+cat("Estimate of slope (with uncertainty) \n")
+mod$slope
+
+if (mod$slope[["2.5%"]] > 0){
+  # If lower range (2.5 percentile) of the slope estimate is above zero....
+  cat("Significant increase over time")
+} else if (mod$slope[["97.5%"]] < 0){
+  # If upper range (97.5 percentile) of the slope estimate is below zero....
+  cat("Significant decrease over time")
+} else {
+  cat("No significant change over time")
+}
+
+# Test plot  
 ggplot(dat_test2, aes(x = MYEAR)) +
   geom_point(aes(y = CB7_up), color = "red") +
-  geom_point(aes(y = CB7_lo), color = "blue")
+  geom_point(aes(y = CB7_lo), color = "blue") +
+  geom_line(data = mod$plot_data, aes(x, y)) +
+  geom_line(data = mod$plot_data, aes(x, y_lo), linetype = "dashed") +
+  geom_line(data = mod$plot_data, aes(x, y_hi), linetype = "dashed")
 
-mle_fit <- ic_par(cbind(CB7_lo, CB7_up) ~ MYEAR,
-                  model = "ph",
-                  dist = "lnorm",
-                  data = dat_test2)
-summary(mle_fit)
+
+
+#
+# . shorter time series (until 2014) ----      
+#
+
+dat_test2b <- dat_test2 %>% filter(MYEAR <= 2016)
+
+# Model
+mod <- int_linear_qi(data = dat_test2b, x = "MYEAR", y_lo = "CB7_lo", y_up = "CB7_up")
+
+# str(mod,1)
+
+cat("Estimate of slope (with uncertainty) \n")
+mod$slope
+
+
+# Test plot  
+ggplot(dat_test2b, aes(x = MYEAR)) +
+  geom_point(aes(y = CB7_up), color = "red") +
+  geom_point(aes(y = CB7_lo), color = "blue") +
+  geom_line(data = mod$plot_data, aes(x, y)) +
+  geom_line(data = mod$plot_data, aes(x, y_lo), linetype = "dashed") +
+  geom_line(data = mod$plot_data, aes(x, y_hi), linetype = "dashed")
 
 
 
